57a4aed2f90164f9bf22bd2958d8822e172f986a
Ville Oravilkka <ville.oravilkka@fmi.fi>
2023-08-20 17:22:44 +0300

    Update calc parameter logic

--- routes/weatherData.js
+++ routes/weatherData.js
@@ -70,36 +70,82 @@ module.exports = function weatherData(app) {
 
     let r_1d = 0
     let r_1h = null
+    let tmax = -999
+    let tmin = 999
+    let ws_1d = -0.1
+    let wg_1d = -0.1
+    let wg_max_dir = -0.1
+    let ws_max_dir = -0.1
 
     for (let i = 0; i < data.length - 1; i++) {
       if (data[i].fmisid === data[i + 1].fmisid) {
         r_1d = r_1d + data[i]['r_1h']
+
+        if (data[i]['t2m'] && data[i]['t2m'] > tmax) tmax = data[i]['t2m']
+        if (data[i]['t2m'] && data[i]['t2m'] < tmin) tmin = data[i]['t2m']
+        if (data[i]['ws_10min'] && data[i]['ws_10min'] > ws_1d) {
+          ws_1d = data[i]['ws_10min']
+          ws_max_dir = data[i]['wd_10min']
+        }
+        if (data[i]['wg_10min'] && data[i]['wg_10min'] > wg_1d) {
+          wg_1d = data[i]['wg_10min']
+          wg_max_dir = data[i]['wg_10min']
+        }
+
         if ((new Date(data[i]['time'])).getUTCHours() === 0 && (new Date(data[i]['time'])).getUTCMinutes() === 0) {
           r_1h = data[i]['r_1h']
         }
 
         // final key
-        if (i === data.length - 2) {
-          data[i+1]["r_1d"] = Number(r_1d.toFixed(1))
-          data[i+1]['t2mtdew'] = Number((data[i+1]['t2m'] - data[i+1]['dewpoint']).toFixed(1))
-          if ((new Date(data[i+1]['time'])).getUTCHours() === 0 && (new Date(data[i+1]['time'])).getUTCMinutes() === 0) {
-            r_1h = data[i+1]['r_1h']
+        if (i === data.length - 1) {
+          if (data[i + 1]['t2m'] > tmax) data[i + 1]['tmax'] = data[i + 1]['t2m']
+          if (data[i + 1]['t2m'] < tmin) data[i + 1]['tmin'] = data[i + 1]['t2m']
+
+          if (data[i+1]['ws_10min'] && data[i+1]['ws_10min'] > ws_1d) {
+            ws_1d = data[i+1]['ws_10min']
+            ws_max_dir = data[i+1]['wd_10min']
           }
-          data[i+1]['r_1h'] = r_1h
-          returnArray.push(data[i+1])
+          if (data[i+1]['wg_10min'] && data[i+1]['wg_10min'] > wg_1d) {
+            wg_1d = data[i+1]['wg_10min']
+            wg_max_dir = data[i+1]['wg_10min']
+          }
+
+          data[i + 1]["r_1d"] = Number(r_1d.toFixed(1))
+          data[i + 1]['t2mtdew'] = Number((data[i + 1]['t2m'] - data[i + 1]['dewpoint']).toFixed(1))
+
+          if ((new Date(data[i + 1]['time'])).getUTCHours() === 0 && (new Date(data[i + 1]['time'])).getUTCMinutes() === 0) {
+            r_1h = data[i + 1]['r_1h']
+          }
+          data[i + 1]['r_1h'] = r_1h
+          returnArray.push(data[i + 1])
         }
+
+
 
       } else {
         data[i]["r_1d"] = Number(r_1d.toFixed(1))
-        data[i]['t2mtdew'] = Number((data[i]['t2m'] - data[i]['dewpoint']).toFixed(1))
+        data[i]['t2mtdew'] = (data[i]['t2m'] && data[i]['dewpoint']) ? Number((data[i]['t2m'] - data[i]['dewpoint']).toFixed(1)) : null
         if ((new Date(data[i]['time'])).getUTCHours() === 0 && (new Date(data[i]['time'])).getUTCMinutes() === 0) {
           r_1h = data[i]['r_1h']
         }
         data[i]['r_1h'] = r_1h
+        data[i]['tmin'] = tmin === 999 ? null : tmin
+        data[i]['tmax'] = tmax === -999 ? null : tmax
+        data[i]['ws_1d'] = ws_1d < 0 ? null : ws_1d
+        data[i]['wg_1d'] = wg_1d < 0 ? null : wg_1d
+        data[i]['ws_max_dir'] = ws_max_dir < 0 ? null : ws_max_dir
+        data[i]['wg_max_dir'] = wg_max_dir < 0 ? null : wg_max_dir
         returnArray.push(data[i])
 
+        tmax = -999
+        tmin = 999
         r_1d = 0
         r_1h = 0
+        ws_1d = -0.1
+        wg_1d = -0.1
+        wg_max_dir = -0.1
+        ws_max_dir = -0.1
+
       }
     }
 
